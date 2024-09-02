326822970be5158bc9c19eface48d16a793ebdfd
Skysayson <jonaz.juan@hotmail.com>
2024-09-02 08:18:55 +0800

    add: zod util

--- lib/utils.ts
+++ lib/utils.ts
@@ -195,15 +195,16 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = z.object({
+export const authFormSchema = (type: string) => z.object({
+  //both
   email: z.string().email(),
   password: z.string().min(8),
-  username: z.string(),
-  firstName: z.string(),
-  lastName: z.string(),
-  address: z.string(),
-  state: z.string(),
-  postalCode: z.string(),
-  dateOfBirth: z.date(),
-  socialSecurityNumber: z.number()
+  //sign up
+  firstName: type === 'sign-in' ? z.string().optional() :  z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() :  z.string().min(3),
+  address: type === 'sign-in' ? z.string().optional() :  z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() :  z.string().min(2).max(2),
+  postalCode: type === 'sign-in' ? z.string().optional() :  z.string().min(3).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() :  z.string().min(3),
+  socialSecurityNumber: type === 'sign-in' ? z.number().optional() : z.string().min(3),
 });
